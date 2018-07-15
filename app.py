9fd652cdbea66092f73a9f7997059f8921d62c7c
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-07-15 14:32:41 +0900

    function login logout
    ・login, logoutの機能を作った．

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session, redirect, make_response,url_for
 import psycopg2
 import psycopg2.extras
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, login_user, UserMixin,logout_user,current_user
 
 app=Flask(__name__)
 # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/diary'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.secret_key = 'ufawifyagwer1742yncs2'
+app.secret_key = 'ufawifyagwer1742yncs1'
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
 
 
 #モデルの定義エリア
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),unique=True)
     password = db.Column(db.String(20))
@@ -30,6 +40,49 @@ def home():
     all_past_post = Diary_table.query.filter(Diary_table.id == 1)
     return render_template("home.html",all_past_post = all_past_post)
 
+#sign in sign up系のルーティング
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.form["username"] and request.form["password"]:
+        username = request.form["username"]
+        password = request.form["password"]
+        newUser = User(username=username,password=password)
+        db.session.add(newUser)
+        db.session.commit()
+        login_user(newUser,True) # ユーザが新規登録されたときは，ログイン状態にする．
+        return redirect("/")
+    else:
+        return "エラーです"
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign_in.html")
+
+@app.route("/login",methods=["POST"])
+def log_in():
+    if request.form["username"] and request.form["password"]:
+        posted_username = request.form["username"]
+
+        user_in_database = User.query.filter_by(username=posted_username).first()
+        if user_in_database and request.form["password"] == user_in_database.password: # 入力されたpasswordが正しい場合
+            login_user(user_in_database,True)
+            return redirect("/")
+        else:
+            return "エラーが発生しました．"
+    else:
+        return "エラーが発生しました．"
+
+@app.route("/logout")
+def log_out():
+    logout_user()
+    return redirect("/")
+
+# end sign_in or sign_up rooting
+
 @app.route("/posting_page")
 def top_page():
     return render_template("posting_page.html")
@@ -45,7 +98,7 @@ def post_page():
     # dbにインスタンスを挿入
     db.session.add(new_page)
     db.session.commit()
-    return render_template("posted_page.html", title=title,contents=contents)
+    return redirect("/")
 
 
 #DBのコマンド
